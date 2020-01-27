dc8bec6730a26f62860daee44b1c774e99c029f9
Gerax Sotelo <gsotelo23@gmail.com>
2020-01-27 16:59:46 -0700

    Add while loop, methods to return books, and print books

--- Models/Library.cs
+++ Models/Library.cs
@@ -16,12 +16,13 @@ namespace library.Models
     {
       AvailableBooks.Add(book);
     }
-    public void Checkout(string selection)
+    // CHECKOUT BOOK
+    public void CheckoutBook(string selection)
     {
       Book selectedBook = ValidateBook(AvailableBooks, selection);
       if (selectedBook == null)
       {
-        Console.WriteLine("Invalid Selection");
+        Console.WriteLine(" Invalid Selection");
         return;
       }
       else
@@ -29,7 +30,25 @@ namespace library.Models
         selectedBook.Available = false;
         CheckedOut.Add(selectedBook);
         AvailableBooks.Remove(selectedBook);
-        Console.WriteLine("Check out successful!");
+        Console.WriteLine(" Check out successful!");
+      }
+    }
+    // RETURN BOOK
+    public void ReturnBook(string selection)
+    {
+      Book selectedBook = ValidateBook(CheckedOut, selection);
+      if (selectedBook == null)
+      {
+        Console.WriteLine(" Invalid Selection");
+        return;
+      }
+      else
+      {
+        selectedBook.Available = true;
+        AvailableBooks.Add(selectedBook);
+        CheckedOut.Remove(selectedBook);
+        Console.Clear();
+        Console.WriteLine(" Book Returned!");
       }
     }
     private Book ValidateBook(List<Book> booklist, string selection)
@@ -53,7 +72,16 @@ namespace library.Models
       {
         Console.WriteLine($" {i + 1}.) {AvailableBooks[i].Title} - {AvailableBooks[i].Author}");
       }
-      Console.WriteLine("\n Select a book number to checkout, (Q)uit, or (R)eturn a book");
+      Console.WriteLine("\n Select a book number to checkout, (Q) to quit, or (R) to return a book");
+    }
+    public void PrintCheckedOut()
+    {
+      Console.WriteLine(" Books Checked Out: ");
+      for (int i = 0; i < CheckedOut.Count; i++)
+      {
+        Console.WriteLine($"{i + 1}.) {CheckedOut[i].Title} - {CheckedOut[i].Author}");
+      }
+      Console.WriteLine(" Select a book number to return, (Q) to quit, or (A) to see available books.");
     }
 
     public Library(string name, string location)
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace library
   {
     static void Main(string[] args)
     {
+      bool inLibrary = true;
+
       Book book1 = new Book("Book Title One", "Author 1");
       Book book2 = new Book("Book Title Two", "Author 2");
       Book book3 = new Book("Book Title Three", "Author 3");
@@ -15,10 +17,48 @@ namespace library
       lib.AddBook(book2);
       lib.AddBook(book3);
       Console.Clear();
-      lib.PrintBooks();
-      string selection = Console.ReadLine();
-      lib.Checkout(selection);
-      //   lib.PrintBooks();
+      Enum activeMenu = Menus.CheckoutBook;
+      while (inLibrary)
+      {
+        switch (activeMenu)
+        {
+          case Menus.CheckoutBook:
+            lib.PrintBooks();
+            break;
+          case Menus.ReturnBook:
+            lib.PrintCheckedOut();
+            break;
+        }
+        string selection = Console.ReadLine();
+        switch (selection.ToLower())
+        {
+          case "q":
+            inLibrary = false;
+            break;
+          case "a":
+            activeMenu = Menus.CheckoutBook;
+            break;
+          case "r":
+            activeMenu = Menus.ReturnBook;
+            break;
+          default:
+            if (activeMenu.Equals(Menus.CheckoutBook))
+            {
+              lib.CheckoutBook(selection);
+            }
+            else
+            {
+              lib.ReturnBook(selection);
+            }
+            break;
+        }
+      }
+    }
+
+    public enum Menus
+    {
+      CheckoutBook,
+      ReturnBook
     }
   }
 }
